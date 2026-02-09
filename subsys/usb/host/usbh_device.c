5514310bc300005c0bb970a1204bb57ca8657307
Dv Alan <zhangyang.shen@nxp.com>
2026-02-09 11:30:12 +0800

    usb: host: Add dynamic configuration matching of supported class
    Modify device initialization to iterate through all available device
    configurations and probe for supported classes, stopping at the first
    configuration that has a matching class driver.
    
    Signed-off-by: Dv Alan <zhangyang.shen@nxp.com>

--- subsys/usb/host/usbh_device.c
+++ subsys/usb/host/usbh_device.c
@@ -501,6 +501,12 @@ void usbh_device_connect(struct usbh_context *const ctx,
 	}
 
 	err = usbh_device_init(udev);
+
+	if (err == -ENOTSUP) {
+		LOG_WRN("Not supported USB device");
+		return;
+	}
+
 	if (err != 0) {
 		LOG_ERR("Failed to init new USB device");
 		if (usbh_device_is_root(ctx, udev)) {
@@ -510,8 +516,6 @@ void usbh_device_connect(struct usbh_context *const ctx,
 		usbh_device_free(udev);
 		return;
 	}
-
-	usbh_class_probe_device(udev);
 }
 
 void usbh_device_disconnect(struct usbh_context *ctx, struct usb_device *udev)
@@ -593,9 +597,23 @@ int usbh_device_init(struct usb_device *const udev)
 
 	LOG_INF("New device with address %u state %u", udev->addr, udev->state);
 
-	err = usbh_device_set_configuration(udev, 1);
-	if (err) {
-		LOG_ERR("Failed to configure new device with address %u", udev->addr);
+	/* FIXME: Avoid multiple setting configuration when device has multiple configurations */
+	/* FIXME: If all configurations are not supported, last configuration is selected */
+	for (unsigned int i = 0; i < udev->dev_desc.bNumConfigurations; i++) {
+		err = usbh_device_set_configuration(udev, i + 1);
+		if (err != 0) {
+			LOG_ERR("Failed to configure new device with address %u", udev->addr);
+			continue;
+		}
+
+		err = usbh_class_probe_device(udev);
+		if (err != 0) {
+			LOG_DBG("Failed to probe device class for configuration %u", i + 1);
+			continue;
+		}
+
+		LOG_INF("Device class probed successfully for configuration %u", i + 1);
+		break;
 	}
 
 error:
